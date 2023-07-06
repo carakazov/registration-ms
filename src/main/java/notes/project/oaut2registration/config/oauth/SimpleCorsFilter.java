22a1ec71328725aba6c6af3b0052e029f8f0b55b
vadim.lebediantsev <yfgjktjy1813>
2023-07-06 17:55:50 +0300

    role changing logic and tests

--- src/main/java/notes/project/oaut2registration/config/oauth/SimpleCorsFilter.java
+++ src/main/java/notes/project/oaut2registration/config/oauth/SimpleCorsFilter.java
@@ -16,7 +16,7 @@ public class SimpleCorsFilter implements Filter {
         IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
         chain.doFilter(req, res);
